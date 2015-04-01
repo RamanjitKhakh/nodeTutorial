f3461227ac0f372010840801074665f3f0baa841
unknown <tue65926@CIS-SE35906.tu.temple.edu>
2015-04-01 18:01:14 -0400

    second

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static('./public'));
 
 app.get('/storytime', function(req, res) {
-	res.send("<p>hey "+req.query.name +" nice to meet you</p>");
+	res.send("<p>hey "+req.query.name +" go "+req.query.verb+" yourself. Nice to meet you</p>");
 });
 
 app.listen(app.get('port'), function() {
