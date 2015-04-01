de16bc0fdad8e93eab1d4d9d12dc8c7d754c130b
unknown <tue65926@CIS-SE35906.tu.temple.edu>
2015-04-01 17:52:21 -0400

    second

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 
 app.get('/', function(req, res) {
-	res.sendfile('./index.html');
+	res.sendfile('./public/index.html');
 });
 
 app.listen(app.get('port'), function() {
