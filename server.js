842ac9eb4b98640bbccc1af9c5a5fb0fafbe9962
unknown <tue65926@CIS-SE35906.tu.temple.edu>
2015-04-01 17:58:46 -0400

    second

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var request = require('request');
 app.set('port', (process.env.PORT || 5000));
-
+app.use(express.static('./public'));
 
 app.get('/storytime', function(req, res) {
 	res.send("<p>hey "+req.query.name +" nice to meet you</p>");
